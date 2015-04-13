8925cd4318223db02586db8365060084ed084c29
lyman wong <lymanwong@gmail.com>
2015-04-12 17:30:56 -0700

    completed module 2 assignment

---
+++ Module_Two_Assignment/Module_Two_Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Module_Two_Assignment
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string evenLines = "XOXOXOXO";
+			string oddLines = "OXOXOXOX";
+			int lines = 0;
+			while(lines <= 7) {
+				if (lines % 2 == 0){
+					Console.Write (evenLines);
+					Console.WriteLine ();
+					lines++;
+				} else {
+					Console.Write (oddLines);
+					Console.WriteLine ();
+					lines++;
+				};
+			}
+		}
+	}
+}
