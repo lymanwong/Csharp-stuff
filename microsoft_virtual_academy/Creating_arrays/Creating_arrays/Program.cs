b3b5ebb56dddef4b8eb4a5dfd6d194bcd9f2d221
lymanwong <lymanwong@gmail.com>
2015-04-18 15:35:03 -0700

    adding 2 files

---
+++ microsoft_virtual_academy/Creating_arrays/Creating_arrays/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creating_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbers = new int[5]; //initialize the array max capacity of 5
+            //numbers[0] = 4;
+            //numbers[1] = 8;
+            //numbers[2] = 15;
+            //numbers[3] = 16;
+            //numbers[4] = 23;
+            //numbers[5] = 42; //will throw an error bc you're trying to add 6 numbers into an array that can only hold 5. 
+
+            //int[] numbers = new int[] { 4,8,15,16,23,42}; //initialize array and set capacity with 5 also
+
+            //Console.WriteLine(numbers[1].ToString());
+            //Console.ReadLine();
+
+            //string[] names = new string[] { "Eddie", "Alex", "Michael", "David Lee" };
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+                
+            //}
+            //Console.ReadLine();
+
+            string zig = "You can have a long string " +
+                "by using the plus key.";
+
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char zigChar in charArray)
+                Console.Write(zigChar);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ microsoft_virtual_academy/While_Iterations/While_Iterations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Iterations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader myReader = new System.IO.StreamReader("values.txt");
+            string line = "";
+
+            while (line != null)
+            {
+                line = myReader.ReadLine();
+                if (line != null)
+                    Console.WriteLine(line);
+            }
+            myReader.Close();
+            Console.ReadLine();
+        }
+    }
+}
